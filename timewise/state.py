2a49a39a2cc19a1e8b165070c3cb8edab1f740a2
Valentin-Prieto <valentin.prieto@mail.utdt.edu>
2024-11-13 14:23:54 -0300

    arreglo

--- timewise/state.py
+++ timewise/state.py
@@ -84,17 +84,16 @@ class State(rx.State):
         # while i < 5:
         #     print(split_docs[i], type(split_docs[i]))
         #     i+=1
-        # texts = []
-        # for doc in split_docs:
-        #     print("CONTENT", doc.page_content)
-
-        #     texts.append(doc.page_content)
+        texts = []
+        for doc in split_docs:
+            #print("CONTENT", doc.page_content)
+            texts.append(doc.page_content)
         # print("TEXTS", texts)
         #texts = [doc.page_content if hasattr(doc, 'page_content') else doc for doc in split_docs]
         #texts = [" ".join(doc) if isinstance(doc, list) else doc for doc in texts]
         print("SPLIT", split_docs)
         embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
-        db = FAISS.from_documents(split_docs, embeddings)
+        db = FAISS.from_documents(texts, embeddings)
         db.save_local('vectorstore/db_faiss')
         
         return db
